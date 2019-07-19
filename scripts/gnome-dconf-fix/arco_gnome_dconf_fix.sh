6198649dec8ba0cfadf27d7d7285d02900b861af
Marco Obaid <marco.obaid@gmail.com>
2019-07-19 08:23:42 -0500

    Added gnome fix for dconf database

---
+++ scripts/gnome-dconf-fix/arco_gnome_dconf_fix.sh
@@ -0,0 +1,144 @@
+#!/bin/bash
+
+#set -e
+
+##################################################################################################################
+# Author: 	Marco Obaid
+# GitHub:   https://github.com/marcoobaid
+##################################################################################################################
+#
+#   Script to Fix Arcolinux Gnome default settings in dconf database in /etc/skel and user home ~/.config/dconf
+#  
+######################################################################################################################
+# Arcolinux stores its configurations and gnome settings in a default directory in /etc/skel. Some of these settings
+# default to settings that may not meet the prefrence of the entire user community. For example, the default keyboard
+# mapping is set to "Belgian" by default. Furthermore, running the "skel" alias, used to update the system settings,
+# will reset the user customizations to the default stock settings, overwritting dconf desired defaults.
+# This script is intended to automate the process of restoring any desired personal settings to /etc/skel and user
+# home Gnome dconf database. It will do this process in the following order:
+# 		1. Backup the existing /etc/skel and ~/.config/dconf user databases (in case a rollback is needed).
+#		2. Dump the existing /etc/skel and ~/.config/dconf user database to text files, for comparison purposes.
+#		3. Upload any desired settings to the user home dconf database.
+# To upload settings, the respective dconf path must be explicitly stated in the script. For example:
+#	conf_change_path1="/org/gnome/desktop/input-sources/"
+# Then, drop the settings in a file within the backup folder and explicitly define it in the script. For example:
+# 	dconf_load_settings1=$backup_dir/load_dconf_settings1
+######################################################################################################################
+
+if [ ! -d "$HOME/backup" ]; then
+	mkdir $HOME/backup
+	echo ""
+	echo "###############################################################################"
+	echo "~/backup folder does not exist. Exiting now!"
+	echo "###############################################################################"
+	echo""
+	exit 0
+fi
+
+# Let's set some variables
+backup_dir=$HOME/backup
+archive_dir=$backup_dir/archive/
+
+cd $backup_dir
+
+# Backup dconf databases from user home and /etc/skel
+if [ ! -d "$archive_dir" ]; then
+	mkdir $archive_dir
+fi
+mv -f $backup_dir/backup_dconf* $archive_dir
+rm -rf $backup_dir/backup_dconf* 
+cp /etc/skel/.config/dconf/user $backup_dir/backup_dconf_skel_$(date +%Y.%m.%d-%H.%M.%S)
+cp $HOME/.config/dconf/user $backup_dir/backup_dconf_user_$(date +%Y.%m.%d-%H.%M.%S)
+echo ""
+echo "###############################################################################"
+echo "dconf databases in /etc/skel and user home dir have been successfully backed up"
+echo "###############################################################################"
+echo ""
+ls -la $HOME/backup/backup*
+echo ""
+
+# Let's dump skel and user settings to do a comparison
+cp /etc/skel/.config/dconf/user $HOME/.config/dconf/skel_user
+printf %s\\n "user-db:skel_user" > db_profile
+DCONF_PROFILE=$backup_dir/db_profile dconf dump / > dump_dconf_skel_settings.txt 
+rm -f db_profile
+rm -f $HOME/.config/dconf/skel_user
+printf %s\\n "user-db:user" > db_profile
+DCONF_PROFILE=$backup_dir/db_profile dconf dump / > dump_dconf_user_settings.txt
+rm -f db_profile
+
+echo "################################################################################################"
+echo "Text dump of skel and user home dconf databases have been successfully generated"
+echo "Please go to the backup folder to review dump_dconf_skel_settings and dump_dconf_user_settings"
+echo "Meld is a good tool to do compare the user settings to the default Arcolinux settings"
+echo "################################################################################################"
+echo ""
+ls -la $HOME/backup/dump*
+echo ""
+
+# Let's load new settings to dconf in user home
+dconf_change_path1="/org/gnome/desktop/input-sources/"
+dconf_load_settings1=$backup_dir/load_dconf_settings1
+dconf_change_path2="/org/gnome/settings-daemon/plugins/power/"
+dconf_load_settings2=$backup_dir/load_dconf_settings2
+
+echo -n "Would you like to load settings to dconf? y/n "
+echo ""
+echo "**********************************************************************"
+echo "(I will be looking for load_dconf_settings file in ~/backup folder)"
+echo "Press y to continue or any key to exit ..."
+read answer
+
+if [ $answer = "y" ]; then
+	dconf load $dconf_change_path1 < $dconf_load_settings1
+	dconf load $dconf_change_path2 < $dconf_load_settings2
+	dconf update
+	echo ""
+	echo "###############################################################################"
+	echo "New dconf settings have been loaded into dconf user database. Please check now!"
+	echo "###############################################################################"
+	echo ""
+else
+	echo ""
+	echo "###############################################################################"
+	echo "Existing script ... No further action required ... Doublcheck your $backup_dir"
+	echo "###############################################################################"
+	echo ""
+fi
+
+# Let's upload the same settings to /etc/skel/.config/dconf/user database
+
+echo -n "Would you like to load settings to dconf in /etc/skel? y/n "
+echo ""
+echo "**********************************************************************"
+echo "(I will be looking for load_dconf_settings file in ~/backup folder)"
+echo "Press y to continue or any key to exit ..."
+read answer
+
+if [ $answer = "y" ]; then
+	mv -f $HOME/.config/dconf/user $HOME/.config/dconf/user_backup
+	cp /etc/skel/.config/dconf/user $HOME/.config/dconf/user
+	dconf load $dconf_change_path1 < $dconf_load_settings1
+	dconf load $dconf_change_path2 < $dconf_load_settings2
+	dconf update
+	echo ""
+	echo "###############################################################################"
+	echo "Replacing the dconf database in skel folder ..."
+	echo "###############################################################################"
+	echo ""
+	sudo sudo mv -f $HOME/.config/dconf/user /etc/skel/.config/dconf/
+	mv -f $HOME/.config/dconf/user_backup $HOME/.config/dconf/user
+	dconf update
+	echo ""
+	echo "###############################################################################"
+	echo "New dconf settings have been loaded into dconf skel database. Please check now!"
+	echo "*** Backup copies of dconf databases in skel and user home are in ~/backup ***"
+	echo "###############################################################################"
+	echo ""
+else
+	echo ""
+	echo "###############################################################################"
+	echo "Existing script ... No further action required ... Doublcheck your $backup_dir"
+	echo "###############################################################################"
+	echo ""
+fi
---
+++ scripts/gnome-dconf-fix/arco_gnome_dconf_fix_skel.sh
@@ -0,0 +1,114 @@
+#!/bin/bash
+
+#set -e
+
+##################################################################################################################
+# Author: 	Marco Obaid
+# GitHub:   https://github.com/marcoobaid
+##################################################################################################################
+#
+#   Script to upload Arcolinux Gnome preferred settings to dconf database in /etc/skel only 
+#  
+######################################################################################################################
+# When you run skel command after updates from Arcolinux, the default dconf database /etc/skel will overwrite the 
+# dconf database in your home folder (~/.config/dconf) and your default keyboard settings will revert to "Belgian".
+# This script can be integrated into the skel command so that your preferred settings will be upload to the dconf
+# database in /etc/skel before, it is copied to your home folder. This should alleviate any frustration of have to 
+# reconfigure your settings after running a skel command. The same script can be used to upload other prefences. You
+# will need to place the preferences for each path in dconf database in a seperate file and update the script.
+#
+# How to use the script
+# #####################
+# Make sure the script has "execute" permissions and run it. The script will create a "~/backup" folder to store backups
+# of the original dconf database before it uploads any settings. You can always restore the original database (copy/paste).
+# The script expects the preferred settings to be in the backup folder. There are two settings that this script will
+# change by default. 1) It will make gnome keyboard mapping "US" (you can change it to your liking) 2) it will implement
+# a Power setting to prevent the system from hypernating after 20 minutes of idle time. This setting is turned on by default
+# and causes the VirtualBox VM to freeze.
+#
+# If previous backups of dconf databases exist, the script will move the old ones into "archive" folder to keep things clean.
+#############################################################################################################################
+
+if [ ! -d "$HOME/backup" ]; then
+	mkdir $HOME/backup
+	echo ""
+	echo "###############################################################################"
+	echo "~/backup folder does not exist. Exiting now!"
+	echo "###############################################################################"
+	echo""
+	exit 0
+fi
+
+# Let's set some variables
+backup_dir=$HOME/backup
+archive_dir=$backup_dir/archive/
+
+# Let's define preferred dconf settings to be uploaded
+#
+# Keyboard Map
+dconf_change_path1="/org/gnome/desktop/input-sources/"
+dconf_load_settings1=$backup_dir/load_dconf_settings1
+#
+# Do not hypernate ater 20 minutes
+dconf_change_path2="/org/gnome/settings-daemon/plugins/power/"
+dconf_load_settings2=$backup_dir/load_dconf_settings2
+#
+
+cd $backup_dir
+
+# Backup dconf databases from user home and /etc/skel
+if [ ! -d "$archive_dir" ]; then
+	mkdir $archive_dir
+fi
+mv -f $backup_dir/backup_dconf* $archive_dir 2>/dev/null
+rm -rf $backup_dir/backup_dconf* 2>/dev/null
+cp /etc/skel/.config/dconf/user $backup_dir/backup_dconf_skel_$(date +%Y.%m.%d-%H.%M.%S) 2>/dev/null
+cp $HOME/.config/dconf/user $backup_dir/backup_dconf_user_$(date +%Y.%m.%d-%H.%M.%S) 2>/dev/null
+echo ""
+echo "###############################################################################"
+echo "dconf databases in /etc/skel and user home dir have been successfully backed up"
+echo "###############################################################################"
+echo ""
+ls -la $HOME/backup/backup*
+echo ""
+
+
+# Let's upload the peferred settings to /etc/skel/.config/dconf/user database
+
+echo -n "Would you like to load settings to dconf in /etc/skel? y/n "
+echo ""
+echo "**********************************************************************"
+echo "(I will be looking for load_dconf_settings file(s) in ~/backup folder)"
+echo "Press y to continue or any key to exit ..."
+read answer
+
+if [ $answer = "y" ]; then
+	mv -f $HOME/.config/dconf/user $HOME/.config/dconf/user_backup 2>/dev/null
+	cp /etc/skel/.config/dconf/user $HOME/.config/dconf/user 2>/dev/null
+	dconf load $dconf_change_path1 < $dconf_load_settings1
+	dconf load $dconf_change_path2 < $dconf_load_settings2
+	dconf update
+	echo ""
+	echo "###############################################################################"
+	echo "Replacing the dconf database in skel folder ..."
+	echo "###############################################################################"
+	echo ""
+	sudo sudo mv -f $HOME/.config/dconf/user /etc/skel/.config/dconf/ 2>/dev/null
+	mv -f $HOME/.config/dconf/user_backup $HOME/.config/dconf/user 2>/dev/null
+	dconf update
+	echo ""
+	echo "###############################################################################"
+	echo "New dconf settings have been loaded into dconf skel database. Please check now!"
+	echo "*** Backup copies of dconf databases in skel and user home are in ~/backup ***"
+	echo "###############################################################################"
+	echo ""
+	ls -la /etc/skel/.config/dconf/user*
+	echo ""
+	echo "******************** DONE !!! ***********************"
+else
+	echo ""
+	echo "###############################################################################"
+	echo "Existing script ... No further action required ... Doublcheck your $backup_dir"
+	echo "###############################################################################"
+	echo ""
+fi
